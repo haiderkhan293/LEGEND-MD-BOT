591cdc154e36f2e6d90c87110d4454730e90d36a
haiderkhan293 <haidergilgite75@gmail.com>
2025-11-16 15:54:42 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Legend MD Bot',
+  author: '‎',
+  botName: "Legend MD Bot,
+  botOwner: 'Legend Haider', // Your name
+  ownerNumber: '923109559495', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.3",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
